04ba594a2b4b08a3ef1a343da878d6435e1388a3
Booba Skaya <booba.skaya@gmail.com>
2013-05-16 07:39:10 +0200

    Change db location to follow github project name

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/booba/workspace/mysite/sqlitedb.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/booba/workspace/django-tuto/sqlitedb.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
