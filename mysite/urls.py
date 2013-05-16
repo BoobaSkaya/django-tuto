46432cb306be40392015a04fbe27258b70181dcf
Booba Skaya <booba.skaya@gmail.com>
2013-05-16 07:38:37 +0200

    Fix error

--- mysite/urls.py
+++ mysite/urls.py
@@ -58,5 +58,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls')),
 )
